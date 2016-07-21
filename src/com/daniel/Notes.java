697966a8f1c5d195c1d106bebeacbcbc254e733b
DanielJ18 <atochala20@gmail.com>
2016-07-21 14:19:41 -0400

    07/21/16 2:13

---
+++ src/com/daniel/Notes.java
@@ -0,0 +1,10 @@
+//package com.daniel;
+//
+//public class Notes 
+//{
+//	for (int i = 0; i < 5; i++) //  
+//	{
+//	System.out.println();
+//	}
+//
+//}
--- src/com/daniel/Whatever.java
+++ src/com/daniel/Whatever.java
@@ -4,8 +4,55 @@ public class Whatever {
 
 	public static void main(String[] args) 
 	{
-		// :)
+		// You are awesome:)
+		
+		//Bang(7) Buzz(5) Fizz(3)
 
+		for(int i = 100; i > 0; i--)                
+			
+		{
+			      if (((i % 3) == 0) && ((i % 5) == 0) && ((i % 7)==0))       
+			    	  {
+			    	  System.out.print("FizzBuzzBang");    
+			    	  }
+			      
+			      else if (((i % 3) == 0) && ((i % 5) == 0))
+			      {
+			      System.out.print("FizzBuzz"); // 
+			      }
+			      
+			      else if (((i % 3) == 0) && ((i % 7) == 0))
+			      {
+			    	  System.out.print("FizzBang"); // 
+			      }
+			      
+			      else if (((i % 5) == 0) && ((i % 7) == 0))
+			      {
+			    	  System.out.println("BuzzBang");
+			      }
+			      
+			      else if (((i % 3) == 0))
+			      {
+			    	  System.out.println("Fizz");
+			      }
+			      
+			      else if (((i % 5) == 0))
+			      {
+			    	  System.out.println("Buzz");
+			      }
+			      
+			      else if (((i % 7) == 0))
+			      {
+			    	  System.out.println("Bang");
+			      }
+			      
+			      else
+			      {
+			    	  System.out.println(i); 
+			      }
+
+		
+		}
 	}
-
 }
+
